5f423a4af3e9e4263468d9d1be4e6644fa6ca8da
Matas <matas.vaitkus19@gmail.com>
2021-10-21 14:42:39 +0300

    7uzduotis
    Pridedu varototoju duomenu saugoji faile

--- 7uzduotis/UserDatabase.cs
+++ 7uzduotis/UserDatabase.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _7uzduotis
 {
     class UserDatabase
     {
         public List<Vartotojas> Vartotojai { get; private set; }
+        private readonly string _path = "../../vartotojai.txt";
 
         public UserDatabase()
         {
             Vartotojai = new List<Vartotojas>();
             Vartotojai.Add(new Vartotojas("pirmas", "123456"));
             Vartotojai.Add(new Vartotojas("antras", "password"));
+            File.WriteAllText(_path, "pirmas 123456\nantras password\n");
+
         }
 
         //metodas tikrina ar egzistuoja vartotojas, jei egzistuoja, grazina jo index'a vartotoju liste, jei ne grazina -1
@@ -41,6 +45,10 @@ namespace _7uzduotis
         public void PridetiVartotoja(string name, string pasw)
         {
             Vartotojai.Add(new Vartotojas(name, pasw));
+            using(StreamWriter sw = File.AppendText(_path))
+            {
+                sw.WriteLine($"{name} {pasw}");
+            }
         }
     }
 }
