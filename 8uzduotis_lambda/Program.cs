2b66f201ea41ad479d2c03b2ac0a467cb4d88135
Matas <matas.vaitkus19@gmail.com>
2021-10-21 22:46:02 +0300

    8uzduotis_lambda
    ikeliu 8 uzduoti padaryta su lambda expresionu

---
+++ 8uzduotis_lambda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8uzduotis_lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentuFailas = "../../Studentai.txt";
+            string destytojuFailas = "../../Destytojai.txt";
+
+            Rusiuotojas sort = new Rusiuotojas(studentuFailas, destytojuFailas);
+
+            sort.IsvetiRusiuota();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
