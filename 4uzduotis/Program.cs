09ce19b05936109e79aae93b5eb8c64b210279e4
Matas <matas.vaitkus19@gmail.com>
2021-10-21 11:53:15 +0300

    Ikeliu pabaigta 4 uzduoti

---
+++ 4uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordDatabase zodziuLaikmena = new WordDatabase();
+
+            zodziuLaikmena.Ivedimas();
+
+            Console.WriteLine();
+
+            zodziuLaikmena.Isvedimas();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4uzduotis/WordDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4uzduotis
+{
+    class WordDatabase
+    {
+        private List<string> _Zodziai;
+
+        public WordDatabase()
+        {
+            _Zodziai = new List<string>();
+        }
+
+        public void Ivedimas()
+        {
+            Console.WriteLine("Pradedamas ivedimas, pasikartojantys zodziai nebus issaugoti\nNorint baigti ivedima suvesti \"baigti\"");
+            string zodis = "";
+            while(zodis != "baigti")
+            {
+                Console.WriteLine("Iveskite zodi:");
+                zodis = Console.ReadLine();
+                if (_Zodziai.Contains(zodis) && zodis != "baigti")
+                {
+                    Console.WriteLine($"Zodis \"{zodis}\" kartojasi ir nebus issaugotas");
+                }
+                else if(zodis != "baigti")
+                {
+                    _Zodziai.Add(zodis);
+                    Console.WriteLine($"\"{zodis}\" buvo issaugotas!");
+                }
+            }
+        }
+        
+        public void Isvedimas()
+        {
+            Console.WriteLine("Isaugoti zodziai:");
+            foreach(string zodis in _Zodziai)
+            {
+                Console.WriteLine(zodis);
+            }
+        }
+    }
+}
