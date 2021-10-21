c963ceb9b80c14bdf1407adb983ded4787c75f4b
Matas <matas.vaitkus19@gmail.com>
2021-10-21 14:22:50 +0300

    7 uzduotis
    Vietoj saugojimo faile padaryta vartotoju saugojimas liste. Programa pabaigus darba isspauzdina consoleje visu vartotoju username

---
+++ 7uzduotis/Svetaine.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7uzduotis
+{
+    class Svetaine
+    {
+        public string Pavadinimas {get;}
+        public UserDatabase Duombaze { get; }
+
+        public Svetaine(string pav)
+        {
+            Pavadinimas = pav;
+            Duombaze = new UserDatabase();
+        }
+
+        public void PradetiDarba()
+        {
+            Console.WriteLine($"Sveiki atvyke i svetaine \"{Pavadinimas}\"");
+            
+
+            string pasirinkimas = "";
+            while (pasirinkimas != "atsijungti")
+            {
+                Console.WriteLine("Iveskite ka norite daryti:");
+                Console.WriteLine("prisijungti/registruotis/atsijungti");
+                pasirinkimas = Console.ReadLine().ToLower();
+                if(pasirinkimas == "prisijungti")
+                {
+                    Prisijungti();
+                }
+                else if(pasirinkimas == "registruotis")
+                {
+                    Registruotis();
+                }
+                else if(pasirinkimas == "atsijungti")
+                {
+                    Console.WriteLine("Viso gero!");
+                }
+                else
+                {
+                    Console.WriteLine("Blogas pasirinkimas!!! Kartokite");
+                }
+            }
+
+            foreach(Vartotojas user in Duombaze.Vartotojai)
+            {
+                Console.WriteLine(user.GautiUsername());
+            }
+        }
+
+
+        private void Prisijungti()
+        {
+            string username, password;
+            bool kartoti = true;
+
+            while (kartoti) { 
+                Console.Write("Iveskite savo username: ");
+                username = Console.ReadLine();
+                Console.Write("Iveskite savo password: ");
+                password = Console.ReadLine();
+
+                int userIndex = Duombaze.ArEgzistuoja(username);    //jei userIndex -1, vadinasi tokio vartotojo nera
+                bool prisijungta;
+
+            
+                if(userIndex < 0)       //tikrina ar egzistuoja vartotojas su pasirinktu username
+                {
+                    Console.WriteLine("Vartotojo su tokiu username nera");
+                    kartoti = Kartoti();                //paklausia vartotojo ar jis nori kartoti prisijungima
+                }
+                else            //jei egzistuoja, patikrina ar atitinka slaptazodziai
+                {
+                    prisijungta = Duombaze.TikrintiPassword(password, userIndex);
+                    if (prisijungta == true)
+                    {
+                        Console.WriteLine("Sekmingai prisijungete!!!");
+                        kartoti = false;            //sustabdo prisijungima
+                    }
+                    else
+                    {
+                        Console.WriteLine("Blogai ivestas vartotojo slaptazodis!");
+                        kartoti = Kartoti();            //paklausia vartotojo ar jis nori kartoti prisijungima
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void Registruotis()
+        {
+            string username, password;
+            bool kartoti = true;
+
+            while (kartoti)
+            {
+                Console.Write("Iveskite nauja username: ");
+                username = Console.ReadLine();
+                Console.Write("Iveskite nauja password: ");
+                password = Console.ReadLine();
+
+                int userIndex = Duombaze.ArEgzistuoja(username);    //jei userIndex -1, vadinasi tokio vartotojo nera
+                
+                
+
+
+                if (userIndex < 0)       //tikrina ar neegzistuoja vartotojas su pasirinktu username
+                {
+                    Duombaze.PridetiVartotoja(username, password);
+                    Console.WriteLine("Sekmingai uzsiregistravote");
+                    kartoti = false;
+                }
+                else            //jei egzistuoja, patikrina ar atitinka slaptazodziai
+                {
+                    Console.WriteLine("Toks username jau uzimtas!");
+                    kartoti = Kartoti();                //paklausia vartotojo ar jis nori kartoti prisijungima
+                }
+            }
+        }
+
+
+        private bool Kartoti()
+        {
+            string pasirinkimas;
+
+            while (true) { 
+                Console.WriteLine("Ar norite kartoti?  taip/ne");
+                pasirinkimas = Console.ReadLine().ToLower();
+                if(pasirinkimas == "taip")
+                {
+                    return true;
+                }
+                if(pasirinkimas == "ne")
+                {
+                    return false;
+                }
+                Console.WriteLine("Ivestas blogas pasirinkimas! Bandykite is naujo!");
+            }
+
+        }
+    }
+}
---
+++ 7uzduotis/UserDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7uzduotis
+{
+    class UserDatabase
+    {
+        public List<Vartotojas> Vartotojai { get; private set; }
+
+        public UserDatabase()
+        {
+            Vartotojai = new List<Vartotojas>();
+            Vartotojai.Add(new Vartotojas("pirmas", "123456"));
+            Vartotojai.Add(new Vartotojas("antras", "password"));
+        }
+
+        //metodas tikrina ar egzistuoja vartotojas, jei egzistuoja, grazina jo index'a vartotoju liste, jei ne grazina -1
+        public int ArEgzistuoja(string name)    
+        {
+            foreach(Vartotojas user in Vartotojai)
+            {
+                if(user.GautiUsername() == name)
+                {
+                    return Vartotojai.IndexOf(user);
+                }
+            }
+            
+            return -1;
+        }
+
+        public bool TikrintiPassword(string pasw , int index)
+        {
+            return Vartotojai[index].TikrintiPassword(pasw);
+
+            
+        }
+
+        public void PridetiVartotoja(string name, string pasw)
+        {
+            Vartotojai.Add(new Vartotojas(name, pasw));
+        }
+    }
+}
